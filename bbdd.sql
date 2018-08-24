76ba79287d199d7414325eab789652a2fd571b9d
Alex <alexxsur@gmail.com>
2018-08-23 21:38:43 -0500

    nombre columna descripcion

--- bbdd.sql
+++ bbdd.sql
@@ -5,7 +5,7 @@ USE curso_angular4;
 CREATE TABLE productos(
 id int(255) auto_increment not null,
 nombre varchar(255),
-description text,
+descripcion text,
 precio varchar(255),
 imagen varchar(255),
 CONSTRAINT pk_productos PRIMARY KEY(id)
